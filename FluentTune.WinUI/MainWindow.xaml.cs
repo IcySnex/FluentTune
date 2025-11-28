aea6a70deccafd18b71bd97a263fdfbdca3b9993
IcySnex <81762750+IcySnex@users.noreply.github.com>
2025-11-28 21:18:52 +0100

    Initial commit

---
+++ FluentTune.WinUI/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml;
+
+namespace FluentTune.WinUI;
+
+public sealed partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FluentTune
+A native YouTube Music desktop client built with C# & WinUI3 - fast, sleek, & modern.
